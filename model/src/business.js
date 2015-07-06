3fceffc266f010913dab663cd684a6280ed56ded
zhangxiaobin <977277822@qq.com>
2015-07-06 15:31:45 +0800

    js mvc

---
+++ model/src/business.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2015/7/6.
+ */
+
+
+(function () {
+    if (define) {
+        define("app/run", function (require, exports, module) {
+            var MVC = require("mvc");
+            module.run = function (data) {
+                require(["mvc"], function (MVC) {
+                    MVC.config([{
+                        modelType: "Student",
+                        renderData: data,
+                        view: "#tpl",
+                        render: "body"
+                    }]);
+                })
+            }
+
+            return module;
+        })
+    }
+})()
---
+++ model/src/service.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2015/7/6.
+ */
+
+
+(function(){
+    if(require){
+        require(["app/run"],function(app){
+            app.run([{ id:"1" , name : "2"  } , { id:"3" , name : "4" }]);
+        })
+    }
+})()
