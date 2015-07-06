cb84db8c5b00ed955ab4a6b7991316a58ee03b36
zhangxiaobin <977277822@qq.com>
2015-07-06 10:42:54 +0800

    js mvc

--- model/src/mvc.js
+++ model/src/mvc.js
@@ -2,136 +2,40 @@
  * Model. View. Control framework.
  * Created by zxb on 2015/6/23.
  */
-
-var Model = {}, View = {};
 (function () {
-    var hasOwn = Object.prototype.hasOwnProperty;
-    var Class = function (proConfig, consConfig) {
-        var pro = {}, cons = {};
-        if (proConfig) {
-            for (var p in proConfig) {
-                if (hasOwn.call(proConfig, p)) {
-                    pro[p] = proConfig[p];
+    if (define) {
+        define("mvc", function (require, exports, module) {
+            var Class = require("mvc-class").Class;
+            var Model = require("mvc-model");
+            var View = require("mvc-view");
+
+
+            module.config = function (defaultConfig) {
+                if (defaultConfig) {
+                    var dcs = [];
+                    if (typeof defaultConfig == "array") {
+                        dcs = defaultConfig;
+                    }
+                    else if (typeof defaultConfig == "object") {
+                        dcs = [defaultConfig];
+                    }
+                    for (var i = 0, len = dcs.length; i < len; i++) {
+                        var _self = dcs[i];
+                        var modelType = _self.modelType;
+                        var renderData = _self.renderData;
+                        var view = _self.view;
+                        var render = _self.render;
+
+                        var modelClass = Model.create({registerType: modelType, renderData: renderData});
+                        var modelObject = modelClass.instance();
+
+                        var viewClass = View.create({model: modelObject, view: view, render: render});
+                        var viewObject = viewClass.instance();
+                    }
                 }
-            }
-        }
-        if (consConfig) {
-            for (var p in consConfig) {
-                if (hasOwn.call(consConfig, p)) {
-                    cons[p] = consConfig[p];
-                }
-            }
-        }
-        return (function () {
-            var func = function () {
-            };
-            var proto = func.prototype;
-            var constructor = func.constructor;
-            for (var p in pro) {
-                proto[p] = pro[p];
-            }
-            for (var p in cons) {
-                func[p] = cons[p];
-            }
-            return proto.constructor;
-        })();
-    };
 
-
-    if (Model) {
-        Model.stack = [];
-        Model.create = function (defaultConfig) {
-            if (defaultConfig || typeof defaultConfig == "object") {
-                var _Model = Class({
-                    __registerType: defaultConfig.registerType,
-                    __renderData: defaultConfig.renderData,
-                    get: function (attrId, attrValue) {
-                        var that = this, renderData = that.__renderData, resultArr = [];
-                        if (!attrId && !attrValue) {
-                            return renderData;
-                        }
-
-                        if (renderData) {
-                            for (var i = 0, len = renderData.length; i < len; i++) {
-                                var t = renderData[i];
-                                if (attrId && attrValue) {
-                                    if (t[attrId] === attrValue) {
-                                        resultArr.push(t);
-                                    }
-                                } else if (attrId) {
-                                    if (t[attrId]) {
-                                        resultArr.push(t);
-                                    }
-                                }
-                            }
-                            return resultArr;
-                        }
-                    }
-                }, {
-                    instance: function () {
-                        var o = new this;
-                        Model.stack.push(o);
-                        return o;
-                    }
-                });
-                Model.stack.push(_Model);
-                return _Model;
             }
-        }
-
-        View.stack = [];
-        View.create = function (defaultConfig) {
-            if (defaultConfig || typeof defaultConfig == "object") {
-                var _View = Class({
-                    __modelType: defaultConfig.modelType,
-                    __view: defaultConfig.view,
-                    __render: defaultConfig.render,
-                    render: function () {
-                        var that = this, modelType = that.__modelType, view = that.__view, render = that.__render;
-                        if (Model.stack && window.juicer) {
-                            var data = [],html;
-                            for (var i = 0, len = Model.stack.length; i < len; i++) {
-                                var _self = Model.stack[i];
-                                if(_self.__registerType === modelType){
-                                    data.push(_self.__renderData);
-                                }
-                            }
-                            html = juicer(view,data);
-                            console.log(html);
-                        } else {
-                            new Error("juicer template or model is not find");
-                        }
-                    },
-                    refresh: function () {
-                    }
-                }, {
-                    render: function () {
-                        var o = new this;
-                        View.stack[o];
-                        return o;
-                    }
-                });
-            }
-        }
+            return module;
+        })
     }
-})();
-
-(function () {
-    var A = Model.create({registerType: "Student", renderData: [{id: 1, name: "zs"}, {id: 2, name: "zs2"}]});
-    var B = Model.create({registerType: "Class", renderData: [{id: 3, name: "zs"}, {id: 4, name: "zs2"}]});
-
-    var V = View.create({modelType: "Student", view: "aaaa", render: "dddd"});
-
-    var a = A.instance();
-    var b = B.instance();
-
-    console.log(a.get("id", 1))
-    console.log(A.instance())
-    console.log(B.instance())
-    //a.instance();
-    // b.instance();
-    //console.log(a.get("id", 1))
-    //console.log(b.get("id", 3))
 })()
-
-
--- model/src/view.js
+++ model/src/view.js
@@ -6,27 +6,21 @@
 (function(){
     if(define){
         define("mvc-view",function(require, exports, module){
-            var model = require("mvc-model");
             var template = require("mvc-juicer");
+            var Class = require("mvc-class").Class;
+            var node = require("node");
             module.stack = [];
             module.create = function (defaultConfig) {
                 if (defaultConfig || typeof defaultConfig == "object") {
                     var _View = Class({
-                        __modelType: defaultConfig.modelType,
+                        __object: defaultConfig.model,
                         __view: defaultConfig.view,
                         __render: defaultConfig.render,
                         render: function () {
-                            var that = this, modelType = that.__modelType, view = that.__view, render = that.__render;
-                            if (model.Model.stack && template) {
-                                var data = [],html;
-                                for (var i = 0, len = Model.stack.length; i < len; i++) {
-                                    var _self = Model.stack[i];
-                                    if(_self.__registerType === modelType){
-                                        data.push(_self.__renderData);
-                                    }
-                                }
-                                html = juicer(view,data);
-                                console.log(html);
+                            var that = this, modelType = that.__modelType, view = that.__view, render = that.__render,__renderData = that.__object.__renderData;
+                            if (__renderData && template) {
+                                html = template(view,__renderData);
+                                node.all(render).append(html);
                             } else {
                                 new Error("juicer template or model is not find");
                             }
@@ -34,14 +28,17 @@
                         refresh: function () {
                         }
                     }, {
-                        render: function () {
+                        instance: function () {
                             var o = new this;
-                            View.stack[o];
+                            o.render();
                             return o;
                         }
                     });
+                    module.stack.push(_View);
+                    return _View;
                 }
             }
+            return module;
         })
     }
 })()
